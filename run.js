a05a2d839360a22993d6bddaac44a8df3ca072a1
win11905 <donothavemoney@gmail.com>
2019-11-08 23:06:33 +0700

    init commit

---
+++ run.js
@@ -0,0 +1,20 @@
+const admin = require("firebase-admin");
+const axios = require("axios");
+const config = require("./config");
+
+const serviceAccount = require(config.FIREBASE_CREDENTIAL_FILE);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+const query = db
+  .collection("submissions")
+  .orderBy("timestamp")
+  .limit(1)
+  .where("status", "==", "in_queue");
+query.onSnapshot(snapshot => {
+  axios.post(config.BASE_URL, {});
+});
