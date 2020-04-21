0d164854cb8ae641b757be0fb9f9bbced3860ed6
win11905 <donothavemoney@gmail.com>
2020-04-22 00:49:30 +0700

    Use http instead axios

--- run.js
+++ run.js
@@ -1,20 +1,28 @@
 const admin = require("firebase-admin");
-const axios = require("axios");
+const http = require("http");
 const config = require("./config");
 
 const serviceAccount = require(config.FIREBASE_CREDENTIAL_FILE);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
+
+const options = {
+  host: "localhost",
+  port: config.PORT,
+  path: "/",
+  method: "POST",
+};
 
 const query = db
   .collection("submissions")
   .orderBy("timestamp", "desc")
   .limit(1)
   .where("status", "==", "in_queue");
-query.onSnapshot(snapshot => {
-  axios.post(config.BASE_URL, {});
+query.onSnapshot((snapshot) => {
+  const request = http.request(options);
+  request.write("");
 });
